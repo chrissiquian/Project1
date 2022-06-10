992d37abbc67e848a8297481d3cc590167a9b905
chrissiquian <107235416+chrissiquian@users.noreply.github.com>
2022-06-10 01:10:37 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //** Created on May 12, 2022, 5:31 PM
-//** Purpose : Project 1
-// 
+//** Purpose :
+//    */
 
     //System Libraries  
 #include <iostream> //Input/Output Library
@@ -14,7 +14,10 @@ using namespace std;
 
 void welcome();
 void newGame();
-int dicesum(int, int);
+
+int dicesum(int a, int b=0, int c=0){
+    return(a + b + c);
+}
 
 int main(int argc, char** argv) {
 
@@ -32,37 +35,38 @@ int main(int argc, char** argv) {
     char repeat = 'y';
         
 
-    score.open(file1, ios::out);                                             //Records score by utilizing file input/output
+    score.open(file1, ios::out);                                            //Records score by utilizing file input/output
     srand(static_cast<unsigned int>(time(0)));                              //Srand to generate random number for dice roll
     dice1 = dice2 = roll2 = roll = 0;
 
     welcome();
 
     do {
-        newGame();                                    //Shows start of new game
+        newGame();                                                          //Shows start of new game
+
         dice1 = rand() % 6 + 1;                                             //1st Dice for first rolls
         dice2 = rand() % 6 + 1;                                             //2nd Dice for first rolls
-        roll = dicesum(dice1, dice2);                                               //Roll amount for first roll
+        roll = dicesum(dice1, dice2);                                       //Roll amount for first roll
         cout << "You rolled " << roll << "." << endl;
 
-        if (roll == 7 || roll == 11) {                                       //1st roll Win conditions
+        if (roll == 7 || roll == 11) {                                      //1st roll Win conditions
             cout << "Winner !" << endl;
             win++;
         }
 
-        else if (roll == 2 || roll == 3 || roll == 12) {                      //1st roll Lose conditions
+        else if (roll == 2 || roll == 3 || roll == 12) {                    //1st roll Lose conditions
             cout << "Craps! You lose!" << endl;
             lose++;
         }
 
         else {
             bool reRoll;                                                    //Boolean statement for while condition
-            do {                                                             //Do while loop for reroll loop
-                cout << "Starting 'Point' Roll..." << endl;                   //Point roll
+            do {                                                            //Do while loop for reroll loop
+                cout << "Starting 'Point' Roll..." << endl;                 //Point roll
                 dice3 = rand() % 6 + 1;                                     //1st dice for second rolls
                 dice4 = rand() % 6 + 1;                                     //2nd dice for second rolls
-                roll2 = dicesum(dice3, dice4);                                      //Roll amount for second roll
-                if (roll2 == 7) {                                            //Second roll Lose conditions
+                roll2 = dicesum(dice3, dice4);                              //Roll amount for second roll
+                if (roll2 == 7) {                                           //Second roll Lose conditions
                     cout << "Re-rolling..." << endl;
                     cout << "You re-rolled a " << roll2 << endl;
                     cout << "Craps! You Lose!" << endl;
@@ -70,13 +74,13 @@ int main(int argc, char** argv) {
                     reRoll = false;                                         //Exits reroll loop
                 }
 
-                else if (roll2 == roll) {                                     //If 1st and second roll are the same, winner condition
+                else if (roll2 == roll) {                                   //If 1st and second roll are the same, winner condition
                     cout << "Winner !" << " You re-rolled a " << roll2 << " to match your point " << roll << endl;
                     win++;
                     reRoll = false;
                 }
 
-                else {                                                       //Else statement if roll is neither a win or lose, otherwise known as point roll
+                else {                                                      //Else statement if roll is neither a win or lose, otherwise known as point roll
                     cout << "Re-rolling..." << endl;
                     cout << "You re-rolled a " << roll2 << ". You need a "; //Informs player of current roll and needed roll to win
                     cout << roll << " to win." << endl;
@@ -84,19 +88,26 @@ int main(int argc, char** argv) {
                     cout << "Press enter to continue." << endl;             //Press Enter to continue reroll
                     cin.ignore();
                 }
-            } while (reRoll);                                            //Reroll while statement is true
+            } while (reRoll);                                               //Reroll while statement is true
         }
 
         cout << "Would you like to play again? (Y/N)" << endl;              //Statement asking if player would like to play again
         cin >> repeat;
-    } while (repeat == 'y' || repeat == 'Y');                                //While loop for repeat of a new game
+    } while (repeat == 'y' || repeat == 'Y');                               //While loop for repeat of a new game
 
-    cout << "Thank you for playing." << endl;                           //Exit statement
-    score << "Wins: " << win << endl;                                   //Records score of player in a file
+    if (repeat == 'n' || repeat == 'N'){
+    cout << "Thank you for playing." << endl;                               //Exit statement
+    score << "Wins: " << win << endl;                                       //Records score of player in a file
     score << "Losses: " << lose << endl;
-    score.close();                                                      //Closes file after writing.
-
+    score.close();                                                          //Closes file after writing.
     return 0;
+    }
+    
+    else{
+        cout << "ERROR! Invalid Input." << endl;
+        cout << "The program will now terminate." << endl;
+        exit(0);
+    }
 }
 
 void welcome(){
@@ -109,13 +120,7 @@ void welcome(){
 void newGame(){
     static int count;
     count++;
-    string newmsg = "********************* NEW GAME **********************";
+    string newmsg = "************************ NEW GAME ************************";
     cout << newmsg << endl;
     cout << "Game: " << count << endl << endl;
-}
-
-int dicesum(int a, int b){
-    int c;
-    c = a + b;
-    return c;
 }
